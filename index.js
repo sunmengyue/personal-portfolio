fe2dcbabdf766eea70bca517885ea15e3c87e126
bardrabbit <bardrabbit@gmail.com>
2021-06-27 19:56:56 -0400

    make navbar and home section responsive

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
   $(window).scroll(function () {
     if (this.scrollY > 20) {
+      console.log('scrolled');
       $('.navbar').addClass('sticky');
     } else {
       $('.navbar').removeClass('sticky');
     }
+  });
+
+  // toggle menue bar button
+  $('.menu-btn').click(function () {
+    $('.navbar .menu').toggleClass('active');
+    $('.menu-btn i').toggleClass('active');
   });
 });
