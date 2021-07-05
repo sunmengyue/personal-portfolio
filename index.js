ee362e22565fc4b6890ba74ab64b23f71067a74b
bardrabbit <bardrabbit@gmail.com>
2021-07-04 20:43:10 -0400

    add slide up button

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 $(document).ready(function () {
   $(window).scroll(function () {
-    if (this.scrollY > 20) {
-      $('.navbar').addClass('sticky');
-    } else {
-      $('.navbar').removeClass('sticky');
-    }
+    this.scrollY > 20
+      ? $('.navbar').addClass('sticky')
+      : $('.navbar').removeClass('sticky');
+
+    this.scrollY > 500
+      ? $('.scroll-up-btn').addClass('show')
+      : $('.scroll-up-btn').removeClass('show');
+  });
+
+  // slide up script
+  $('.scroll-up-btn').click(function () {
+    $('html').animate({ scrollTop: 0 });
   });
 
   // toggle menue bar button
